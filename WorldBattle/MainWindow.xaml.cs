4d252746671d8d3a66d845e84d3948d59ca5bb99
teodoramz <amzteodor@gmail.com>
2021-10-27 10:03:11 +0300

    modified:   WorldBattle/MainWindow.xaml 	modified:   WorldBattle/MainWindow.xaml.cs

--- WorldBattle/MainWindow.xaml.cs
+++ WorldBattle/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
 
 namespace WorldBattle
 {
@@ -20,11 +22,146 @@ namespace WorldBattle
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NetworkStream stream;
+        public String version = "1.0";
         public MainWindow()
         {
             InitializeComponent();
         }
+        /*
+        * Reads message sent through the network stream
+        * and returns the data as a String
+        */
+        private String ReadMessage()
+        {
+            Byte[] bytes = new Byte[256];
+            String data = null;
+            Int32 i = stream.Read(bytes, 0, bytes.Length);
+            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+            return data;
+        }
 
-        
+        /*
+        * Takes a string and sends the message
+        * through the network stream
+        */
+        private void WriteMessage(String data)
+        {
+            Byte[] bytes = new Byte[256];
+            bytes = System.Text.Encoding.ASCII.GetBytes(data);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+        /*
+        * Sends a message through the network stream to notify the
+        * other player when they disconnect
+        */
+        private void SendDisconnectMessage()
+        {
+            if (stream.CanRead)
+            {
+                WriteMessage("Disconnected");
+                stream.Close();
+            }
+        }
+
+        private void HostButton_Click(object sender, RoutedEventArgs e)
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = Convert.ToInt32(portTextBox.Text);
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                infoLabel.Content = "Waiting for connection...";
+
+                TcpClient client = server.AcceptTcpClient();
+                infoLabel.Content = "Connected!";
+
+                // Get a stream object for reading and writing
+                this.stream = client.GetStream();
+
+                String data = ReadMessage();
+
+                if (data == this.version)
+                {
+                    WriteMessage("Connected");
+
+                   
+                    
+                    SendDisconnectMessage();
+                }
+                else
+                {
+                    WriteMessage("Invalid version");
+                }
+
+                this.Show();
+            }
+            catch (SocketException err)
+            {
+                infoLabel.Content = "SocketException: " + err;
+            }
+            finally
+            {
+                // Stop listening for new clients
+                server.Stop();
+            }
+        }
+
+        private void guestButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Create a TcpClient
+                string ip = ipTextBox.Text;
+                Int32 port = Convert.ToInt32(portTextBox.Text);
+                TcpClient client = new TcpClient(ip, port);
+
+                infoLabel.Content = "Connecting to host...";
+
+                // Get a client stream for reading and writing.
+                this.stream = client.GetStream();
+
+                WriteMessage("1.0");
+
+                String data = ReadMessage();
+
+                if (data == "Connected")
+                {
+                    infoLabel.Content = "Connected!";
+                    
+                }
+                else
+                {
+                    if (data == "Invalid version")
+                    {
+                        infoLabel.Content = "Wrong version!";
+                    }
+                    else
+                    {
+                        infoLabel.Content = data;
+                    }
+                    stream.Close();
+                }
+
+                this.Show();
+
+                SendDisconnectMessage();
+
+                // Close everything
+                client.Close();
+            }
+            catch (ArgumentNullException err)
+            {
+                infoLabel.Content = "ArgumentNullException: " + err;
+            }
+            catch (SocketException err)
+            {
+                infoLabel.Content = "Could not connect to host";
+            }
+        }
     }
 }
